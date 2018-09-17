9107682e1320cc5ba528ae2e7d4b2211a8891554
Adam Zhou <adzhou218@gmail.com>
2018-09-17 12:11:44 -0400

    first commit

---
+++ bubblesort.js
@@ -0,0 +1,19 @@
+class bubbleSort {
+  constructor(arr) {
+    this.arr = arr;
+  }
+
+  swap(){
+
+  }
+
+  compare(){
+
+  }
+
+  bubbleSort(){
+
+  }
+
+
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,40 @@
+
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+});
+
+
+describe('Bubble Sort', function(){
+  let unsortedArray = [5,4,3,2,1];
+  let bubbleSortObj = new bubbleSort(unsortedArray);
+  let sortedArray = [1,2,3,4,5];
+
+  it('sorts an array', function(){
+    expect(bubbleSortObj.bubbleSort()).toEqual(sortedArray);
+  });
+
+  it ('calls swap function the correct number of times', function() {
+
+    spyOn(bubbleSortObj, 'swap').and.callThrough();
+
+    bubbleSortObj.bubbleSort();
+    expect(bubbleSortObj.swap.calls.count()).toEqual(10);
+  })
+
+  it ('calls the compare function the correct number of times', function() {
+
+    spyOn(bubbleSortObj, 'compare').and.callThrough();
+
+    bubbleSortObj.bubbleSort();
+    expect(bubbleSortObj.compare.calls.count()).toEqual(10);
+  })
+
+});
+
+
+describe('Bubble Sort', function(){
+  it('sorts a randomly assorted array', function(){
+    expect( bubbleSort([8,4,6,20,5])).toEqual([4,5,6,8,20]);
+  });
